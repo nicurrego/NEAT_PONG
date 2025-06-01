bdbe7814e9a2ba78a83949dc43fc555d0331a5b5
Nicolas Urrego <nicurrego@gmail.com>
2025-06-02 05:49:16 +0900

    feat: add a NEAT configuration to tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,8 +1,7 @@
 import pygame
 from pong import Game
-
-width, height = 700, 500
-window = pygame.display.set_mode((width, height))
+import neat
+import os
 
 class PongGame:
     def __init__(self, window, width, height):
@@ -35,7 +34,10 @@ class PongGame:
     pygame.quit()
 
 
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
 
-
-
-
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet,neat.DefaultStagnation,
+                           config_path)
