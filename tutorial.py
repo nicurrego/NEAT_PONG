ecba18e4a61a0bb9d6d6b168d6a402657df2b582
Nicolas Urrego <nicurrego@gmail.com>
2025-06-02 06:00:42 +0900

    feat: add the GA with the eval_genomes function)

--- tutorial.py
+++ tutorial.py
@@ -33,6 +33,21 @@ class PongGame:
 
     pygame.quit()
 
+def eval_genomes(genomes, config):
+    width, height = 700, 500
+    window = pygame.display.set_mode((width, height))
+
+    for i, (genome_id1, genome1) in enumerate(genomes):
+        if i == len(genomes) - 1: 
+            break
+        genome1.fitness = 0
+        for genome_id2, genome2 in genomes[i+1:]:
+            genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
+            game = PongGame(window, width, height)
+            game.train_ai(genome1, genome2, config)
+            
+
+
 def run_neat(config):
     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-27')
     p = neat.Population(config)
@@ -40,6 +55,8 @@ def run_neat(config):
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(1))
+
+    winner = p.run(eval_genomes, 50)
     
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
