4a396162a647d3c2da3488866c90cf086a3a23b8
Nicolas Urrego <nicurrego@gmail.com>
2025-06-02 05:50:31 +0900

    feat: implemented a function to run NEAT

--- tutorial.py
+++ tutorial.py
@@ -33,7 +33,14 @@ class PongGame:
 
     pygame.quit()
 
-
+def run_neat(config):
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-27')
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(1))
+    
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config.txt")
@@ -41,3 +48,4 @@ if __name__ == "__main__":
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet,neat.DefaultStagnation,
                            config_path)
+    run_neat(config)
