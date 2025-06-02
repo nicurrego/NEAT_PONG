4d29c43b04459a4d58639bf93683dc40b2f99ec6
Nicolas Urrego <nicurrego@gmail.com>
2025-06-02 12:00:55 +0900

    feat: implement neural network decision logic for left and right paddle movement

--- tutorial.py
+++ tutorial.py
@@ -44,7 +44,25 @@ class PongGame:
                     quit()
 
             output1 = net1.activate((self.left_paddle.y, self.ball.y, abs(self.left_paddle.x - self.ball.x)))
+            decision1 = output1.index(max(output1))
+            
+            if decision1 == 0:
+                pass
+            elif decision1 == 1:
+                self.game.move_paddle(left=True,up=True)
+            else:
+                self.game.move_paddle(left=True, up=False)                
+
             output2 = net2.activate((self.rigth_paddle.y, self.ball.y, abs(self.rigth_paddle.x - self.ball.x)))
+            decision2 = output2.index(max(output2))
+
+            if decision2 == 0:
+                pass
+            elif decision2 == 1:
+                self.game.move_paddle(left=False,up=True)
+            else:
+                self.game.move_paddle(left=False, up=False)                
+
             print(output1, output2)
 
             game_info = self.game.loop()
@@ -75,7 +93,7 @@ def eval_genomes(genomes, config):
 
 
 def run_neat(config):
-    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-27')
+    # p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-27')
     p = neat.Population(config)
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
